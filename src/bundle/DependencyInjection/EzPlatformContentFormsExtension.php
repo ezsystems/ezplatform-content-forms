1d1e3ccd96c4c06d206d527b428f0d8bb140a8f2
Maciej Kobus <maciej.kobus@ez.no>
2019-10-29 11:15:49 +0100

    EZP-30837: Fixed default template configuration

--- src/bundle/DependencyInjection/EzPlatformContentFormsExtension.php
+++ src/bundle/DependencyInjection/EzPlatformContentFormsExtension.php
@@ -9,26 +9,15 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformContentFormsBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Yaml\Yaml;
 
-class EzPlatformContentFormsExtension extends Extension implements PrependExtensionInterface
+class EzPlatformContentFormsExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $configFile = __DIR__ . '/../Resources/config/kernel.yaml';
-        $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ezpublish', $config);
-        $container->addResource(new FileResource($configFile));
     }
 }
