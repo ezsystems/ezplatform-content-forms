7d2815915fe25789b032c7de3cc5f5d2dd1c7466
Dawid Parafiński <dawid.parafinski@ez.no>
2020-12-04 12:33:13 +0100

    EZEE-3350: Added support for saving additional image data from adminUI (#36)

---
+++ src/lib/Form/Transformer/JsonToArrayTransformer.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformContentForms\Form\Transformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+final class JsonToArrayTransformer implements DataTransformerInterface
+{
+    public function transform($value)
+    {
+        if ($value === null) {
+            return '';
+        }
+
+        try {
+            $encoded = json_encode((object) $value, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $exception) {
+            throw new TransformationFailedException(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+
+        return $encoded;
+    }
+
+    public function reverseTransform($value)
+    {
+        if ($value === null) {
+            return [];
+        }
+
+        try {
+            $decoded = json_decode($value, true, 512, JSON_THROW_ON_ERROR);
+        } catch (\JsonException $exception) {
+            throw new TransformationFailedException(
+                $exception->getMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+
+        return $decoded;
+    }
+}
